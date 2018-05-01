24635e2a4b6827c24d6579b33c2bae02e3f07089
Matthew Donald Stevenson <mattdst@hotmail.com>
2018-05-01 12:32:03 -0600

    Added function to increase puckCount based on time in stream

--- functions/index.js
+++ functions/index.js
@@ -191,6 +191,34 @@ exports.wildUserAppears = functions.https.onRequest((request, response) => {
     });
 });
 
+
+exports.puckIncrease = functions.database.ref('{playersRoot}/{channelId}/{playerId}/puckCount').onUpdate(event => {
+    /*Increase PuckCount based on time in stream*/
+    var playerRef = db.ref(`${playersRoot}/${event.params.channelId}/${event.params.playerId}`);
+    var puckIncrease = Date.now() - db.ref(`${playersRoot}/${event.params.channelId}/${event.params.playerId}/lastSeen`).val();
+    puckIncrease /= 60000; //conversion to minutes player has been in stream
+    if (puckIncrease <= 1 && event.data.val() <= 0) {
+        var newPucks = 5;
+    }
+    else if (event.data.val() <= 0) {
+        newPucks = pucksIncrease * 5;
+    }
+    else {
+        newPucks = puckIncrease + event.data.val();
+    }
+    return playerRef.once('value').then(snapshot => {
+        var responseBody = {
+            'puckCount': newPucks
+        };
+        console.log("sending the following: " + JSON.stringify(responseBody));
+        return response.set('Access-Control-Allow-Origin', '*')
+            .json(responseBody);
+    }).catch(reason => {
+        console.log(reason);
+        return response.sendStatus(500);
+    });
+});
+
 exports.puckUpdate = functions.database.ref('{playersRoot}/{channelId}/{playerId}/puckCount').onWrite((data, context) => {
     // generate and sign JWT
     var encodedKey = functions.config().twitch.key;
@@ -208,6 +236,7 @@ exports.puckUpdate = functions.database.ref('{playersRoot}/{channelId}/{playerId
             send: ["*"]
         }
     };
+
     var signedToken = jwt.sign(token, Buffer.from(encodedKey, 'base64'), { 'noTimestamp': true });
     var opaqueRef = db.ref(`${playersRoot}/${context.params.channelId}/${context.params.playerId}/opaqueUserId`);
     var opaqueUserId;
@@ -247,7 +276,7 @@ exports.puckUpdate = functions.database.ref('{playersRoot}/{channelId}/{playerId
         return rp(options);
     }).catch(reason => {
         console.log(reason);
-    });
+        });
 });
 
 exports.pointsUpdate = functions.database.ref('{playersRoot}/{channelId}/{playerId}/points').onWrite((data, context) => {
